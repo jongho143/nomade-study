58191efb08f2ab9e6b6f00b61d99ceca559a3f95
leexiudev <leexiu.dev@gamil.com>
2020-04-07 16:22:33 +0900

    loading state

--- src/App.js
+++ src/App.js
@@ -9,18 +9,7 @@ import Movie from './Movie.js'
 class App extends Component {
 
   state = {
-    greeting:'Hello!',
-    movies: [
-      {
-        title: "Matrix", poster: "https://post-phinf.pstatic.net/MjAxOTA4MjFfMjg0/MDAxNTY2MzQ4ODYxNjM2.dhCDfrjD4gmQAMpqTU2OoPI8Bpbpz--hlO6cWaJXJs0g.Spmg8TZy1V5fFRNhcrRwV5Y6TXvM7JcEJlcRBjAjHAEg.JPEG/81.jpg?type=w1200"
-      },
-      {
-        title: "Oldboy", poster: "https://www.readersnews.com/news/photo/201912/96921_62339_244.jpg"
-      },
-      {
-        title: "star was", poster: "https://post-phinf.pstatic.net/MjAxOTA0MTZfNyAg/MDAxNTU1MzcxODAwOTA1.u9goQ7xANTfZdBhYTmtSK3bgE3yl0u66CHDE-S61CGog.EJ_bfU0Bww6Q6b55j4TeI30RNJpne4-Za2jsTESev5gg.JPEG/sw_ros_main.jpg?type=w1200&type=w1200"
-      }
-    ]
+    
   }
 
   componentDidMount(){
@@ -37,12 +26,17 @@ class App extends Component {
     }, 5000)
   }
     
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
   render() {
     return (
       <div className='App'>
-        {this.state.movies.map((movie, index) => {
-          return<Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
