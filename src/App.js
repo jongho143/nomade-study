81af09c514f93876c0d93c7b23c075692d3c9581
leexiudev <leexiu.dev@gamil.com>
2020-04-07 20:46:07 +0900

    오류 해결은 집에서

--- src/App.js
+++ src/App.js
@@ -8,23 +8,39 @@ import Movie from './Movie.js'
 
 class App extends Component {
 
-state= {}
+  state = {}
 
-  componentDidMount(){
-    fetch('https://yts.mx/api/v2/list_movies.json/sort_by=rating')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+  componentDidMount() {
+    this._getMovies();
   }
-    
+
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie 
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis} 
+      />
     })
     return movies
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.mx/api/v2/list_movies.json/sort_by=rating')
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
 
   render() {
     return (
--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css'
 
 
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return (
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie_Colums">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie-Colums">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map((genre,index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movies_synopsis">
+                    {synopsis}
+                </p>
+            </div>
         </div>
     );
 }
 
 
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
     return (
-        <img src={poster} alt='이미지'></img>
+        <img src={poster} alt={alt} title={alt} className="Movie_Poster" />
     )
 }
 
+function MovieGenres({genre}){
+    return (
+        <span className="Movie_Genre">{genre}</span>
+    )
+}
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenres.propTypes = {
+    genre: PropTypes.string.isRequired
+}
 
 
 export default Movie;
